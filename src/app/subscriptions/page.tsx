47805f7321a9d87576b8ae462313371b307528fe
Rashedul Islam Maruf <mdm326040@gmail.com>
2025-02-13 09:15:47 +0600

    Responsive subscriptions page

--- src/app/subscriptions/page.tsx
+++ src/app/subscriptions/page.tsx
@@ -11,7 +11,7 @@ import User from "./_components/User";
 
 const Subscriptions = () => {
   return (
-    <main className="pb-20">
+    <main className="md:pb-20">
       <section className="p-5 pb-0 relative bg-white-2 rounded-[10px]">
         <Carousel>
           <div className="flex items-center justify-between gap-5 flex-wrap">
@@ -20,7 +20,7 @@ const Subscriptions = () => {
             </h2>
             <div className="flex items-center gap-4">
               <span className="text-sm text-white/60">12 of 84</span>
-              <div className="flex items-center gap-2">
+              <div className="hidden md:flex items-center gap-2">
                 <CarouselPrevious className="bg-white/5 size-9 border-none hover:bg-primary" />
                 <CarouselNext className="bg-white/5 size-9 border-none hover:bg-primary" />
               </div>
@@ -28,32 +28,32 @@ const Subscriptions = () => {
           </div>
 
           <div className="mt-3 pb-4">
-            <CarouselContent className="gap-[30px]">
-              <CarouselItem className="basis-1/3 flex flex-col gap-[10px] divide-y-[1px] divide-white/5">
+            <CarouselContent className="gap-5 md:gap-[30px] w-[calc(100vw-100px)]">
+              <CarouselItem className="basis-1/1.2 md:basis-1/2.5 lg:basis-1/3 flex flex-col gap-[10px] divide-y-[1px] divide-white/5 flex-wrap">
                 <User />
                 <User />
                 <User />
                 <User />
               </CarouselItem>
-              <CarouselItem className="basis-1/3 flex flex-col gap-[10px] divide-y-[1px] divide-white/5">
+              <CarouselItem className="basis-1/1.2 md:basis-1/2.5 lg:basis-1/3 flex flex-col gap-[10px] divide-y-[1px] divide-white/5 flex-wrap">
                 <User />
                 <User />
                 <User />
                 <User />
               </CarouselItem>
-              <CarouselItem className="basis-1/3 flex flex-col gap-[10px] divide-y-[1px] divide-white/5">
+              <CarouselItem className="basis-1/1.2 md:basis-1/2.5 lg:basis-1/3 flex flex-col gap-[10px] divide-y-[1px] divide-white/5 flex-wrap">
                 <User />
                 <User />
                 <User />
                 <User />
               </CarouselItem>
-              <CarouselItem className="basis-1/3 flex flex-col gap-[10px] divide-y-[1px] divide-white/5">
+              <CarouselItem className="basis-1/1.2 md:basis-1/2.5 lg:basis-1/3 flex flex-col gap-[10px] divide-y-[1px] divide-white/5 flex-wrap">
                 <User />
                 <User />
                 <User />
                 <User />
               </CarouselItem>
-              <CarouselItem className="basis-1/3 flex flex-col gap-[10px] divide-y-[1px] divide-white/5">
+              <CarouselItem className="basis-1/1.2 md:basis-1/2.5 lg:basis-1/3 flex flex-col gap-[10px] divide-y-[1px] divide-white/5 flex-wrap">
                 <User />
                 <User />
                 <User />
